d10593650c99a39eead2a0f9f7dc35fa169afebb
beerman595692 <beerman595692@gmail.com>
2026-02-02 15:56:03 +0000

    let featuredResp

--- NewgroundsScript.js
+++ NewgroundsScript.js
@@ -101,9 +101,8 @@ class NGMoviePager extends ContentPager {
 
 class NGFeaturedMoviePager extends ContentPager {
 	currentPage = 1;
-	featuredResp = "";
 	constructor() {
-		featuredResp = http.GET(URL_FEATURED_MOVIES.replace("{0}", "1"), { "X-Requested-With": X_REQUESTED_WITH }, null);
+		let featuredResp = http.GET(URL_FEATURED_MOVIES.replace("{0}", "1"), { "X-Requested-With": X_REQUESTED_WITH }, null);
 		if (featuredResp.isOk)
 		{
 			let videos = [];
