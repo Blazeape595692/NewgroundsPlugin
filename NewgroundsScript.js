a9a1ed6ddce012ad98d65dc554f1c65452123011
beerman595692 <beerman595692@gmail.com>
2026-02-10 20:43:53 +0000

    changed a lot

--- NewgroundsScript.js
+++ NewgroundsScript.js
@@ -7,149 +7,242 @@ const URL_CREATOR_MOVIES = "https://{0}.newgrounds.com/movies";
 const URL_MOVIE_FILE = "https://www.newgrounds.com/portal/video/{0}";
 const URL_FEATURED_MOVIES = "https://www.newgrounds.com/movies/featured?offset={0}&inner=1";
 
-var config = {};
-
-let token
-let local_settings
-
-//Source Methods
-source.enable = function(_confing, settings, savedState){
-	local_settings = settings
+source.enable = function (conf) {
+    /**
+     * @param conf: SourceV8PluginConfig (the NewgroundsConfig.js)
+     */
 }
-source.getHome = function() {
-	return new NGFeaturedMoviePager(1);
-};
+
+source.getHome = function(continuationToken) {
+    /**
+     * @param continuationToken: any?
+     * @returns: VideoPager
+     */
+    const videos = []; // The results (PlatformVideo)
+    const hasMore = false; // Are there more pages?
+    const context = { continuationToken: continuationToken }; // Relevant data for the next page
+    return new NewgroundsHomeVideoPager(videos, hasMore, context);
+}
 
 source.searchSuggestions = function(query) {
-	return [];
-};
+    /**
+     * @param query: string
+     * @returns: string[]
+     */
 
-source.getSearchCapabilities = () => {
+    const suggestions = []; //The suggestions for a specific search query
+    return suggestions;
+}
+
+source.getSearchCapabilities = function() {
+    //This is an example of how to return search capabilities like available sorts, filters and which feed types are available (see source.js for more details) 
 	return {
 		types: [Type.Feed.Mixed],
-		sorts: [Type.Order.Chronological],
-		filters: [ ]
+		sorts: [Type.Order.Chronological, "^release_time"],
+		filters: [
+			{
+				id: "date",
+				name: "Date",
+				isMultiSelect: false,
+				filters: [
+					{ id: Type.Date.Today, name: "Last 24 hours", value: "today" },
+					{ id: Type.Date.LastWeek, name: "Last week", value: "thisweek" },
+					{ id: Type.Date.LastMonth, name: "Last month", value: "thismonth" },
+					{ id: Type.Date.LastYear, name: "Last year", value: "thisyear" }
+				]
+			},
+		]
 	};
-};
+}
 
-source.search = function (query, type, order, filters) {
-	return new NGCreatorPager([], false);
-};
+source.search = function (query, type, order, filters, continuationToken) {
+    /**
+     * @param query: string
+     * @param type: string
+     * @param order: string
+     * @param filters: Map<string, Array<string>>
+     * @param continuationToken: any?
+     * @returns: VideoPager
+     */
+    const videos = []; // The results (PlatformVideo)
+    const hasMore = false; // Are there more pages?
+    const context = { query: query, type: type, order: order, filters: filters, continuationToken: continuationToken }; // Relevant data for the next page
+    return new NewgroundsSearchVideoPager(videos, hasMore, context);
+}
 
 source.getSearchChannelContentsCapabilities = function () {
+    //This is an example of how to return search capabilities on a channel like available sorts, filters and which feed types are available (see source.js for more details)
 	return {
 		types: [Type.Feed.Mixed],
 		sorts: [Type.Order.Chronological],
 		filters: []
 	};
-};
+}
 
-source.searchChannelContents = function (channelUrl, query, type, order, filters) {
-	
-};
+source.searchChannelContents = function (url, query, type, order, filters, continuationToken) {
+    /**
+     * @param url: string
+     * @param query: string
+     * @param type: string
+     * @param order: string
+     * @param filters: Map<string, Array<string>>
+     * @param continuationToken: any?
+     * @returns: VideoPager
+     */
 
-source.searchChannels = function (query) {
-	
-};
+    const videos = []; // The results (PlatformVideo)
+    const hasMore = false; // Are there more pages?
+    const context = { channelUrl: channelUrl, query: query, type: type, order: order, filters: filters, continuationToken: continuationToken }; // Relevant data for the next page
+    return new NewgroundsSearchChannelVideoPager(videos, hasMore, context);
+}
 
-//Channel
+source.searchChannels = function (query, continuationToken) {
+    /**
+     * @param query: string
+     * @param continuationToken: any?
+     * @returns: ChannelPager
+     */
+
+    const channels = []; // The results (PlatformChannel)
+    const hasMore = false; // Are there more pages?
+    const context = { query: query, continuationToken: continuationToken }; // Relevant data for the next page
+    return new NewgroundsChannelPager(channels, hasMore, context);
+}
+
 source.isChannelUrl = function(url) {
-	return url.match("^https:\/\/(?!www\.)[a-zA-Z0-9-]+\.newgrounds\.com\/$");
-};
+    /**
+     * @param url: string
+     * @returns: boolean
+     */
+
+	return REGEX_CHANNEL_URL.test(url);
+}
 
 source.getChannel = function(url) {
-	throw new ScriptException("This is a sample");
-};
-source.getChannelContents = function(url) {
-	throw new ScriptException("This is a sample");
-};
+	return new PlatformChannel({
+		//... see source.js for more details
+	});
+}
 
-//Video
+source.getChannelContents = function(url, type, order, filters, continuationToken) {
+    /**
+     * @param url: string
+     * @param type: string
+     * @param order: string
+     * @param filters: Map<string, Array<string>>
+     * @param continuationToken: any?
+     * @returns: VideoPager
+     */
+
+    const videos = []; // The results (PlatformVideo)
+    const hasMore = false; // Are there more pages?
+    const context = { url: url, query: query, type: type, order: order, filters: filters, continuationToken: continuationToken }; // Relevant data for the next page
+    return new NewgroundsChannelVideoPager(videos, hasMore, context);
+}
+
 source.isContentDetailsUrl = function(url) {
-	return false;
-};
-source.getContentDetails = function(url) {
-	throw new ScriptException("This is a sample");
-};
+    /**
+     * @param url: string
+     * @returns: boolean
+     */
 
-//Comments
-source.getComments = function (url) {
-	throw new ScriptException("This is a sample");
+	return REGEX_DETAILS_URL.test(url);
+}
+
+source.getContentDetails = function(url) {
+    /**
+     * @param url: string
+     * @returns: PlatformVideoDetails
+     */
+
+	return new PlatformVideoDetails({
+		//... see source.js for more details
+	});
+}
+
+source.getComments = function (url, continuationToken) {
+    /**
+     * @param url: string
+     * @param continuationToken: any?
+     * @returns: CommentPager
+     */
+
+    const comments = []; // The results (Comment)
+    const hasMore = false; // Are there more pages?
+    const context = { url: url, continuationToken: continuationToken }; // Relevant data for the next page
+    return new NewgroundsCommentPager(comments, hasMore, context);
 
 }
 source.getSubComments = function (comment) {
-	throw new ScriptException("This is a sample");
-}
+    /**
+     * @param comment: Comment
+     * @returns: NewgroundsCommentPager
+     */
 
-log("LOADED");
-
-class NGMoviePager extends ContentPager {
-	currentPage = 0;
-
-	constructor(someInfo) {
-		super([], true); //Alternatively, pass first page results in []
-		this.someInfo = someInfo;
+	if (typeof comment === 'string') {
+		comment = JSON.parse(comment);
 	}
 
+	return getCommentsPager(comment.context.claimId, comment.context.claimId, 1, false, comment.context.commentId);
+}
+
+class NewgroundsCommentPager extends CommentPager {
+    constructor(results, hasMore, context) {
+        super(results, hasMore, context);
+    }
+
+    nextPage() {
+        return source.getComments(this.context.url, this.context.continuationToken);
+    }
+}
+
+class NewgroundsHomeVideoPager extends VideoPager {
+	constructor(results, hasMore, context) {
+		super(results, hasMore, context);
+	}
+	
 	nextPage() {
-		currentPage += 1;
-		const myNewResults = [];
-		this.results = myNewResults;
-		this.hasMore = true; //Or false if last page
+		return source.getHome(this.context.continuationToken);
 	}
 }
 
-class NGFeaturedMoviePager extends ContentPager {
-	currentPage = 1;
-	constructor() {
-		let featuredResp = ""
-		featuredResp = http.GET(URL_FEATURED_MOVIES.replace("{0}", "1"), { "X-Requested-With": X_REQUESTED_WITH }, null);
-		if (featuredResp.isOk)
-		{
-			let videos = [];
-			let featuredContent = domDOMParser.parseFromString(featuredResp.body.content);
-			let featuredVideos = featuredContent.querySelector('.portalsubmission-cell');
-			featuredVideos.forEach(function(x) {
-				let videoMetaData = x.querySelector('a');
-				authorName = x.querySelector('.card-title span').text().toLowerCase().replace("by ", "");
-				videos.push(new PlatformVideo({
-					id: new PlatformID(PLATFORM, videoMetaData.getAttribute('data-video-playback'), config.id),
-					thumbnails: new Thumbnails([
-						new Thumbnail(x.querySelector('.card-img').getAttribute("src"), 720)
-					]),
-					url: videoMetaData.getAttribute('href'),
-					author: new PlatformAuthorLink(new PlatformID(PLATFORM, authorName, config.id), authorName, URL_CREATOR_PROFILE.replace("{0}", authorName), "", null),
-					name: x.getAttribute("title"),
-					viewCount: 0,
-					duration: 0,
-					isLive: false
-				}));
-			});
-			super(videos, true);
-		}
-		super([], false);
+class NewgroundsSearchVideoPager extends VideoPager {
+	constructor(results, hasMore, context) {
+		super(results, hasMore, context);
 	}
-
+	
 	nextPage() {
-		currentPage += 1;
-		const myNewResults = [];
-		this.results = myNewResults;
-		this.hasMore = true; //Or false if last page
+		return source.search(this.context.query, this.context.type, this.context.order, this.context.filters, this.context.continuationToken);
 	}
 }
 
-class NGCreatorPager extends ChannelPager {
-	currentPage = 0;
-
-	constructor(someInfo) {
-		super([], true); //Alternatively, pass first page results in []
-		this.someInfo = someInfo;
+class NewgroundsSearchChannelVideoPager extends VideoPager {
+	constructor(results, hasMore, context) {
+		super(results, hasMore, context);
 	}
-
+	
 	nextPage() {
-		currentPage += 1;
-		const myNewResults = [];
-		this.results = myNewResults;
-		this.hasMore = true; //Or false if last page
+		return source.searchChannelContents(this.context.channelUrl, this.context.query, this.context.type, this.context.order, this.context.filters, this.context.continuationToken);
 	}
 }
+
+class NewgroundsChannelPager extends ChannelPager {
+	constructor(results, hasMore, context) {
+		super(results, hasMore, context);
+	}
+	
+	nextPage() {
+		return source.searchChannelContents(this.context.query, this.context.continuationToken);
+	}
+}
+
+class NewgroundsChannelVideoPager extends VideoPager {
+	constructor(results, hasMore, context) {
+		super(results, hasMore, context);
+	}
+	
+	nextPage() {
+		return source.getChannelContents(this.context.url, this.context.type, this.context.order, this.context.filters, this.context.continuationToken);
+	}
+}
+
